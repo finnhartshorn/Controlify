daa60ed99230737e219fa5d2fa9c69de4da16047
isXander <xander@isxander.dev>
2024-11-10 20:10:07 +0000

    Fix text hover+click being offset

--- src/main/java/dev/isxander/controlify/mixins/feature/screenkeyboard/ChatComponentMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/screenkeyboard/ChatComponentMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ChatComponent.class)
 public abstract class ChatComponentMixin {
@@ -21,21 +23,37 @@ public abstract class ChatComponentMixin {
     @Final
     private Minecraft minecraft;
 
-//    @Definition(id = "floor", method = "Lnet/minecraft/util/Mth;floor(F)I")
-//    @Expression("floor((float) (@(?) - @(40)) / ?)")
-//    @ModifyExpressionValue(method = "render", at = @At("MIXINEXTRAS:EXPRESSION"))
-//    private int modifyChatOffset(int y) {
-//        if (minecraft.screen instanceof ChatScreen chat)
-//            return (int) (y * (1 - ChatKeyboardDucky.getKeyboardShiftAmount(chat)));
-//        return y;
-//    }
+    @Unique private static final int VANILLA_CHAT_PADDING = 40;
+    @Unique private static final int SHIFTED_CHAT_PADDING = 20;
 
-    @ModifyExpressionValue(method = "screenToChatY", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getGuiScaledHeight()I"))
-    private int modifyScreenY(int original) {
+    @Definition(id = "floor", method = "Lnet/minecraft/util/Mth;floor(F)I")
+    @Expression("floor((float) (@(?) - 40) / ?)")
+    @ModifyExpressionValue(method = "render", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private int modifyChatOffset(int y) {
+        if (minecraft.screen instanceof ChatScreen chat)
+            return (int) (y * (1 - ChatKeyboardDucky.getKeyboardShiftAmount(chat)));
+        return y;
+    }
+
+    @ModifyExpressionValue(method = "render", at = @At(value = "CONSTANT", args = "intValue=" + VANILLA_CHAT_PADDING))
+    private int modifyChatToInputPadding(int padding) {
         if (minecraft.screen instanceof ChatScreen chat) {
-            float shiftAmount = 1 - ChatKeyboardDucky.getKeyboardShiftAmount(chat);
-            return (int) (original * shiftAmount + 40 * shiftAmount); // re-add 40 since it is subtracked in the method
+            if (ChatKeyboardDucky.getKeyboardShiftAmount(chat) > 0) {
+                return SHIFTED_CHAT_PADDING;
+            }
         }
-        return original;
+        return padding;
+    }
+
+    @ModifyVariable(method = "screenToChatY", at = @At("HEAD"), argsOnly = true)
+    private double modifyScreenY(double y) {
+        if (minecraft.screen instanceof ChatScreen chat) {
+            float shiftAmount = ChatKeyboardDucky.getKeyboardShiftAmount(chat);
+            if (shiftAmount > 0) {
+                double shiftPixels = shiftAmount * minecraft.getWindow().getGuiScaledHeight() - (VANILLA_CHAT_PADDING - SHIFTED_CHAT_PADDING);
+                return y + shiftPixels;
+            }
+        }
+        return y;
     }
 }
